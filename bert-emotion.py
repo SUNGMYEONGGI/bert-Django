605f40711aaca28fe48f77359099bacf38687116
MyeongGI <smgsmg226@gmail.com>
2022-11-13 20:07:08 +0900

    모델 수정

--- bert-emotion.py
+++ bert-emotion.py
@@ -26,10 +26,69 @@ from kobert.pytorch_kobert import get_pytorch_kobert_model
 from transformers import AdamW
 from transformers.optimization import get_cosine_schedule_with_warmup
 
-
-
 #GPU 사용
-device = torch.device("cuda:0")
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+
+#BERT 모델, Vocabulary 불러오기
+bertmodel, vocab = get_pytorch_kobert_model()
+
+
+import pandas as pd
+df = pd.read_excel('./한국어 감정대화데이터셋.xlsx')
+
+
+df.loc[(df['Emotion'] == "공포"), 'Emotion'] = 0  #공포 => 0
+df.loc[(df['Emotion'] == "놀람"), 'Emotion'] = 1  #놀람 => 1
+df.loc[(df['Emotion'] == "분노"), 'Emotion'] = 2  #분노 => 2
+df.loc[(df['Emotion'] == "슬픔"), 'Emotion'] = 3  #슬픔 => 3
+df.loc[(df['Emotion'] == "행복"), 'Emotion'] = 4  #행복 => 4
+
+data_list = []
+for q, label in zip(df['Sentence'], df['Emotion'])  :
+    data = []
+    data.append(q)
+    data.append(str(label))
+
+    data_list.append(data)
+
+
+#train & test 데이터로 나누기
+from sklearn.model_selection import train_test_split                                         
+dataset_train, dataset_test = train_test_split(data_list, test_size=0.25, random_state=0)
+
+
+class BERTDataset(Dataset):
+    def __init__(self, dataset, sent_idx, label_idx, bert_tokenizer, max_len,
+                 pad, pair):
+        transform = nlp.data.BERTSentenceTransform(
+            bert_tokenizer, max_seq_length=max_len, pad=pad, pair=pair)
+
+        self.sentences = [transform([i[sent_idx]]) for i in dataset]
+        self.labels = [np.int32(i[label_idx]) for i in dataset]
+
+    def __getitem__(self, i):
+        return (self.sentences[i] + (self.labels[i], ))
+
+    def __len__(self):
+        return (len(self.labels))
+
+
+# Setting parameters
+max_len = 20 # 100자 미만으로 입력 / 초과시 그 이상은 읽어 들이지 않음
+batch_size = 64
+warmup_ratio = 0.1
+num_epochs = 50
+max_grad_norm = 1
+log_interval = 200
+learning_rate =  5e-5
+
+#토큰화
+tokenizer = get_tokenizer()
+tok = nlp.data.BERTSPTokenizer(tokenizer, vocab, lower=False)
+
+data_train = BERTDataset(dataset_train, 0, 1, tok, max_len, True, False)
+data_test = BERTDataset(dataset_test, 0, 1, tok, max_len, True, False)
 
 
 class BERTClassifier(nn.Module):
@@ -63,7 +122,7 @@ class BERTClassifier(nn.Module):
     
     
 # model.pt -> 감정분석 bert모델
-bert_model = torch.load('firstproject/data/model.pt')
+bert_model = torch.load('./model.pt')
 
 
 #토큰화
